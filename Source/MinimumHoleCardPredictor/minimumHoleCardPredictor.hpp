bdbb0d572da1b53b68c526e795d6475334055243
Werlen Alexander <Alexwerlen@gmail.com>
2023-11-23 18:12:32 -0300

    implemented minimumHoleCard with cache

--- Source/MinimumHoleCardPredictor/minimumHoleCardPredictor.hpp
+++ Source/MinimumHoleCardPredictor/minimumHoleCardPredictor.hpp
@@ -1,11 +1,12 @@
 /* 
-    Given your known hand and the cards you know from the opponent hand plus their envido points, lets you know the minimum possible truco value that they have in their hole card.
+    Given your known hand and the cards you know from the opponent hand plus their envido points, 
+    lets you know the minimum possible truco value that they have in their hole card.
 */
 
 #pragma once
 
-#include "../Auxiliares/cardUtils.hpp"
-#include "../Auxiliares/deck.hpp"
+#include "../auxiliares/cardUtils.hpp"
+#include "../auxiliares/deck.hpp"
 
 #include <string>
 #include <algorithm>
